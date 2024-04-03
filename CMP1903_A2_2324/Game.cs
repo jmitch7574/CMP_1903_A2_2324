dc2afcc9cc1a3051fbb0c3211f0759fc6233492b
Jake Mitchell <j.mitchell7574@gmail.com>
2024-04-03 17:38:02 +0100

    Made Game Class Abstract

--- CMP1903_A2_2324/Game.cs
+++ CMP1903_A2_2324/Game.cs
@@ -2,8 +2,39 @@ namespace CMP1903_A2_2324;
 
 public abstract class Game
 {
-    public virtual void PlayGame()
+    static public Player PlayerOne;
+    static public Player PlayerTwo;
+    
+    /// <summary>
+    /// Contains the base setup for a playable game
+    /// </summary>
+    /// <param name="playerOne">Player One Object</param>
+    /// <param name="playerTwo">Player Two Object</param>
+    public virtual void PlayGame(Player playerOne, Player playerTwo)
     {
+        PlayerOne = playerOne;
+        PlayerTwo = playerTwo;
         
+        PlayerOne.ResetScore();
+        PlayerTwo.ResetScore();
+        
+    }
+
+    protected void EndGame()
+    {
+        Console.WriteLine("Game Over");
+        
+        if (PlayerOne.Score == PlayerTwo.Score)
+        {
+            Console.WriteLine($"Tie, both players scored {PlayerOne.Score} points");
+        }
+        else
+        {
+            Player winner;
+            if (PlayerOne.Score > PlayerTwo.Score) winner = PlayerOne;
+            else winner = PlayerTwo;
+            
+            Console.WriteLine($"The winner is {winner.Name} with {winner.Score} points");
+        }
     }
 }
