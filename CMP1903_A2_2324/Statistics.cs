bc7c50e041a17aa966e20bd44459b1c360f0b22f
Jake Mitchell <j.mitchell7574@gmail.com>
2024-04-09 14:55:45 +0100

    Rewriting Statistics.cs with saving game states in mind

--- CMP1903_A2_2324/Statistics.cs
+++ CMP1903_A2_2324/Statistics.cs
@@ -1,48 +1,80 @@
+using System.Text.Json;
 using CMP1903_A2_2324.Games;
 
 namespace CMP1903_A2_2324;
 
-public class Statistics
+public static class Statistics
 {
-
-}
-
-public class TurnStats(int turnNo, int playerOneScore, int playerTwoScore)
-{
-    public int TurnNo = turnNo;
-    public int PlayerOneScore = playerOneScore;
-    public int PlayerTwoScore = playerTwoScore;
-
-    public override string ToString()
+    // ---------------------- FILE MANAGING --------------------
+    private static string SerializeGameList(List<Game> game)
     {
-        return $"Turn {TurnNo}, Player One: {playerOneScore}, Player Two: {playerTwoScore}";
-    }
-}
-
-public class GameStats
-{
-    private List<TurnStats> _turnStats;
-
-    public GameStats(List<TurnStats> turnStats)
-    {
-        _turnStats = turnStats;
+        string gameJson = JsonSerializer.Serialize(game);
+        return gameJson;
     }
 
-    public GameStats()
+    public static List<Game> DeserializeGameList(string fileJson)
     {
-        _turnStats = new();
+        List<Game>? games = JsonSerializer.Deserialize<List<Game>>(fileJson);
+        if (games == null)
+            throw new JsonException("JSON data is invalid");
+        return games;
     }
 
-    public void AddTurn(TurnStats turn)
+    public static string ReadFileContents(string filePath)
     {
-        _turnStats.Add(turn);
-    }
-
-    public void OutputGameStats()
-    {
-        foreach (TurnStats turn in _turnStats)
+        if (!File.Exists(filePath)) throw new FileNotFoundException("File does not exist");
+        try
         {
-            Console.WriteLine(turn);
+            string file = File.ReadAllText(filePath);
+            return file;
         }
+        catch
+        {
+            throw new FileLoadException("File could not be loaded");
+        }
+    }
+
+    private static void WriteFileContents(string filePath, string fileContents)
+    {
+        try
+        {
+            File.WriteAllText(filePath, fileContents);
+        }
+        catch
+        {
+            throw new FileLoadException("File could not be written");
+        }
+        
+    }
+
+    public static string[] GetFilesInDirectory(string dirPath)
+    {
+        if (!Directory.Exists(dirPath)) throw new DirectoryNotFoundException("Directory could not be found");
+
+        try
+        {
+            string[] files = Directory.EnumerateFiles(dirPath).ToArray();
+            return files;
+        }
+        catch
+        {
+            throw new FileLoadException("Files in directory could not be read");
+        }
+    }
+
+    public static void WriteGameToFile(Game game)
+    {
+        if (!Directory.Exists("Games/")) Directory.CreateDirectory("Games");
+        string filePath = $"Games/{game.GetType()}-{DateTime.Now:yyyyMMddHHmmss}.json";
+        WriteFileContents(filePath, SerializeGameList([game]));  
+        Console.WriteLine($"Game contents written to {filePath}");
+    }
+
+    public static void WriteGameToFile(List<Game> games)
+    {
+        if (!Directory.Exists("Games/")) Directory.CreateDirectory("Games");
+        string filePath = $"Games/{games[0].GetType()}-{DateTime.Now:yyyyMMddHHmmss}.json";
+        WriteFileContents(filePath, SerializeGameList(games));   
+        Console.WriteLine($"Game contents written to {filePath}");
     }
 }
