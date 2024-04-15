98d65c7140195046d5ce554013b1ce16f0451ebe
Jake Mitchell <j.mitchell7574@gmail.com>
2024-04-15 19:50:17 +0100

    Updated program.cs functionality

--- CMP1903_A2_2324/Program.cs
+++ CMP1903_A2_2324/Program.cs
@@ -5,30 +5,48 @@ public class Program
 {
     // Menu Variables
     private static readonly string MainMenuM = "What would you like to do?";
-    private static readonly string[] MainMenuO = ["Play Game", "Run Statistics", "Run Testing"];
+    private static readonly string[] MainMenuO = ["Play Game", "Run Statistics", "Exit"];
 
     private static readonly string GameMenuM = "What game would you like to play?";
-    private static readonly string[] GameOptions = ["Sevens Out", "Three or More"];
-
     private static readonly string StatsMenuM = "What statistics would you like to run?";
-    private static readonly string TestMenuM = "What would you like to Test?";
-    private static readonly string[] TestAndStatsOptions = ["Sevens Out", "Three or More"];
+    private static readonly string[] GameOptions = ["Sevens Out", "Three or More"];
     
-    private static readonly string StatisticsM = "Would you like to see match statistics?";
-    private static readonly string[] StatisticsO = ["Yes", "No"];
+    private static bool _isRunning = true;
 
+    public static Statistics stats = new();
+    
     /// <summary>
     /// Entry point for program
     /// </summary>
     /// <param name="args"></param>
     public static void Main(string[ ] args)
     {
-        MainMenu();
+        Testing.TestDieCollection();
+        Testing.TestSevensOut();
+        Testing.TestThreeOrMore();
+        
+        // Reset session data so it is not affected by testing
+        stats = new Statistics();
+        
+        while (_isRunning) MainMenu();
+        
+        try
+        {
+            stats.WriteStatsToFile();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("An error occured writing session to file");
+            Console.WriteLine(e);
+        }
     }
 
     /// <summary>
     /// Asks player to choose program operation
     /// </summary>
+    /// <returns>
+    /// A boolean where true means the program was executed and false means the program should be exited
+    /// </returns>
     private static void MainMenu()
     {
         switch (Menu(MainMenuM, MainMenuO))
@@ -37,10 +55,13 @@ public class Program
                 GameMenu();
                 break;
             case "Run Statistics":
+                StatisticsMenu();
                 break;
-            case "Run Testing":
+            case "Exit":
+                _isRunning = false;
                 break;
         }
+        
     }
 
     /// <summary>
@@ -48,15 +69,29 @@ public class Program
     /// </summary>
     private static void GameMenu()
     {
+        Game game = new Game(false, 0);
         switch (Menu(GameMenuM, GameOptions))
         {
             case "Sevens Out":
-                SevensOut so = new SevensOut();
-                so.PlayGame(true);
+                game = new SevensOut(true);
                 break;
             case "Three or More":
-                ThreeOrMore tom = new ThreeOrMore();
-                tom.PlayGame(true);
+                game = new ThreeOrMore(true);
+                break;
+        }
+        game.BeginGame();
+        stats.Session.Add(game);
+    }
+
+    private static void StatisticsMenu()
+    {
+        switch (Menu(StatsMenuM, GameOptions))
+        {
+            case "Sevens Out":
+                stats.Session.AddRange(Statistics.StatsSevensOut());
+                break;
+            case "Three or More":
+                stats.Session.AddRange(Statistics.StatsThreeOrMore());
                 break;
         }
     }
@@ -98,5 +133,19 @@ public class Program
             
 
         }
+    }
+
+    public static int IntInput()
+    {
+        bool validIntInput = false;
+        int intInput = 0;
+        while (!validIntInput)
+        {
+            string input = Console.ReadLine();
+            validIntInput = int.TryParse(input, out intInput);
+            if (!validIntInput) Console.WriteLine("Please enter a valid number");
+        }
+
+        return intInput;
     }
 }
